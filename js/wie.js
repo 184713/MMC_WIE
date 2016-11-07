10ee9c4e8dba971498e25b642ec6ee6ae1cc8f84
Lukasz <lukasz4714@interia.pl>
2016-11-07 21:01:29 +0100

    Brush tool work

--- js/wie.js
+++ js/wie.js
@@ -14,13 +14,43 @@ WIE.prototype._init = function (container, $) {
     var context = this;
     var options = {
         height: 800,
-        width: 600
+        width: 600,
+        colors: [
+            {color: '#000000', name: 'black'},
+            {color: '#FF0000', name: 'red'},
+            {color: '#00FF00', name: 'green'},
+            {color: '#0000FF', name: 'blue'}
+        ]
     };
 
     console.log('[WIE] Started', options);
 
     $('.gp', container).append('<canvas class="canvas"></canvas>');
     this._canvas = new fabric.Canvas(container.find('.canvas')[0], {
+        isDrawingMode: true
     });
     var canvas = this._canvas;
+    canvas.setWidth(options.width);
+    canvas.setHeight(options.height);
+
+    $('.gp', container).append(
+        '<div class="brush-size"><button class="btn set-brush-size" data-size="5" tabindex="1">Brush: small</button>' +
+        '<button class="btn set-brush-size" data-size="10" tabindex="1">Brush: medium</button>' +
+        '<button class="btn set-brush-size" data-size="15" tabindex="1">Brush: big</button></div>'
+    );
+
+    $('.gp', container).append('<div class="brush-color">');
+    $(options.colors).each(function (i, v) {
+        $('.brush-color', container).append('<button class="btn set-brush-color" data-color="' + v.color + '" tabindex="1">Brush color: ' + v.name + '</button>')
+    });
+
+    $('.set-brush-size', container).on('click', function() {
+        canvas.freeDrawingBrush.width = $(this).data('size');
+        $(this).addClass('active').siblings().removeClass('active');
+    }).first().trigger('click');
+
+    $('.set-brush-color', container).on('click', function() {
+        canvas.freeDrawingBrush.color = $(this).data('color');
+        $(this).addClass('active').siblings().removeClass('active');
+    }).first().trigger('click');
 };
