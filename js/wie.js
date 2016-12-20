ffc6c48ea1abef27ff952b399666a4c15bd2a6cd
Adam <az>
2016-12-20 02:28:11 +0100

    UI updates p1

--- js/wie.js
+++ js/wie.js
@@ -13,14 +13,37 @@ WIE.prototype.serializeState = function () {
 WIE.prototype._init = function (container, $) {
     var context = this;
     var options = {
-        height: 800,
-        width: 600
+        width: 800,
+        height: 600,
+        colors: [
+            {color: '#000000', name: 'black'},
+            {color: '#FF0000', name: 'red'},
+            {color: '#00FF00', name: 'green'},
+            {color: '#0000FF', name: 'blue'}
+        ]
     };
 
     console.log('[WIE] Started', options);
 
     $('.gp', container).append('<canvas class="canvas"></canvas>');
     this._canvas = new fabric.Canvas(container.find('.canvas')[0], {
+        isDrawingMode: true
     });
     var canvas = this._canvas;
+    canvas.setWidth(options.width);
+    canvas.setHeight(options.height);
+
+    $(options.colors).each(function (i, v) {
+        $('.brush-color', container).append('<button class="btn set-brush-color" data-color="' + v.color + '" tabindex="1">Brush color: ' + v.name + '</button>')
+    });
+
+    $('.set-brush-size', container).on('click', function() {
+        canvas.freeDrawingBrush.width = $(this).data('size');
+        $(this).addClass('active').siblings().removeClass('active');
+    }).first().trigger('click');
+
+    $('.set-brush-color', container).on('click', function() {
+        canvas.freeDrawingBrush.color = $(this).data('color');
+        $(this).addClass('active').siblings().removeClass('active');
+    }).first().trigger('click');
 };
